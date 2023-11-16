3423124f3ed6d4e91889966c833a7f92d18d131e
buhee1029 <buhee1029@gmail.com>
2023-11-17 03:08:34 +0900

    init setting

---
+++ src/test/java/com/wanted/workwave/WorkwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.workwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
